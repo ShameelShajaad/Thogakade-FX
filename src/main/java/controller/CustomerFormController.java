8adf7b48bb749e037ae148de30f83467f1b32c6c
ShameelShajaad <shameels1228@gmail.com>
2026-01-04 15:58:04 +0530

    added fx ids

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -1,4 +1,85 @@
 package controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class CustomerFormController {
+
+    @FXML
+    private JFXComboBox cmbTitle;
+
+    @FXML
+    private TableColumn colAddress;
+
+    @FXML
+    private TableColumn colCity;
+
+    @FXML
+    private TableColumn colDob;
+
+    @FXML
+    private TableColumn colId;
+
+    @FXML
+    private TableColumn colName;
+
+    @FXML
+    private TableColumn colPostalCode;
+
+    @FXML
+    private TableColumn colProvince;
+
+    @FXML
+    private TableColumn colSalary;
+
+    @FXML
+    private TableView tblCustomers;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtCity;
+
+    @FXML
+    private DatePicker txtDob;
+
+    @FXML
+    private JFXTextField txtId;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtPostalCode;
+
+    @FXML
+    private JFXTextField txtProvince;
+
+    @FXML
+    private JFXTextField txtSalary;
+
+    @FXML
+    void btnAddCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
+    public void initialize(){
+        cmbTitle.getItems().addAll(
+                "Mr",
+                "Ms",
+                "Miss"
+        );
+    }
+
 }
