55314be2d02384358d37140129a4bcdd52a09027
ShameelShajaad <shameels1228@gmail.com>
2026-01-19 14:36:21 +0530

    designed item_form.fxml

--- src/main/java/controller/ItemFormController.java
+++ src/main/java/controller/ItemFormController.java
@@ -1,4 +1,58 @@
 package controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ItemFormController {
+
+    @FXML
+    private TableColumn<?, ?> clmCode;
+
+    @FXML
+    private TableColumn<?, ?> clmDescription;
+
+    @FXML
+    private TableColumn<?, ?> clmPackSize;
+
+    @FXML
+    private TableColumn<?, ?> clmPrice;
+
+    @FXML
+    private TableColumn<?, ?> clmQuantity;
+
+    @FXML
+    private JFXComboBox<?> cmbUnit;
+
+    @FXML
+    private TableView<?> tblItems;
+
+    @FXML
+    private JFXTextField txtDescription;
+
+    @FXML
+    private JFXTextField txtItemCode;
+
+    @FXML
+    private JFXTextField txtPackSize;
+
+    @FXML
+    private JFXTextField txtPrice;
+
+    @FXML
+    private JFXTextField txtQuantity;
+
+    @FXML
+    void btnAddItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
 }
