5c251957081464a04ff32ad2484597abec4683b0
ShameelShajaad <shameels1228@gmail.com>
2026-01-19 14:14:33 +0530

    designed dashboard.fxml

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -2,7 +2,12 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class DashboardController {
 
@@ -11,6 +16,20 @@ public class DashboardController {
 
     @FXML
     void btnCustomerFormOnAction(ActionEvent event) {
+
+        try {
+            URL resource = this.getClass().getResource("/view/customer_form.fxml");
+
+            assert resource != null;
+
+            Parent parent = FXMLLoader.load(resource);
+
+            dashRoot.getChildren().clear();
+            dashRoot.getChildren().add(parent);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
